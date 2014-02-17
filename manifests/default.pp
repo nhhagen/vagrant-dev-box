6f6216baab52a9d9cc5bac8b3797791177552031
Niels Henrik Hagen <nhhagen@gmail.com>
2014-02-17 10:21:30 +0100

    Update default.pp

--- manifests/default.pp
+++ manifests/default.pp
@@ -30,10 +30,10 @@ class nodejs-modules {
 
 class java {
   exec { 'accept_license':
-    command => 'echo debconf shared/accepted-oracle-license-v1-1 select true \
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true \
                | sudo debconf-set-selections \
                && echo debconf shared/accepted-oracle-license-v1-1 seen true \
-               | sudo debconf-set-selections',
+               | sudo debconf-set-selections",
     cwd => '/home/vagrant',
     user => 'vagrant',
     path => '/usr/bin/:/bin/',

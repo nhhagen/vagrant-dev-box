353526a5a0d86d6400981baa8772c89983ad73a2
Niels Henrik Hagen <nhhagen@gmail.com>
2015-02-26 14:09:53 +0100

    Replaced shell setup script with a python one, that should work on Windows

--- README.markdown
+++ README.markdown
@@ -20,7 +20,7 @@ vagrant plugin install vagrant-vbguest
 To start a new project run:
 
 ```bash
-./setup.sh <new repo url> <project-directory>
+python setup.py --origin <new repo url> --directory <project-directory>
 ```
 
 Only one project member needs to do this.
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+import shutil
+import os
+
+
+def setup(args):
+    origin = args.origin
+    project_dir = os.path.abspath(args.directory)
+    shutil.copytree('.', project_dir)
+
+    remove_files = [
+        '.git',
+        'setup.sh',
+        'setup.py',
+        'LICENSE'
+    ]
+
+    for filename in remove_files:
+        filepath = os.path.join(project_dir, filename)
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+        elif os.path.isdir(filepath):
+            shutil.rmtree(os.path.join(project_dir, filename))
+
+    origWD = os.getcwd()
+    os.chdir(project_dir)
+    subprocess.call('git init', shell=True)
+    subprocess.call('git remote add origin ' + origin, shell=True)
+    subprocess.call('git add -A .', shell=True)
+    subprocess.call('git commit -m "Initial commit"', shell=True)
+    subprocess.call('git push --set-upstream origin master', shell=True)
+    os.chdir(origWD)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory',
+                        help='The new directory for the project',
+                        required=True)
+    parser.add_argument('-o', '--origin', help='The git remote for the project',
+                        required=True)
+    args = parser.parse_args()
+    setup(args)
